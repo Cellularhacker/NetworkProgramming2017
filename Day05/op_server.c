35e6bbc58715ab3b3a81f1589c8f1515336e1f8c
Cellularhacker <wva3cdae@gmail.com>
2017-10-09 20:18:44 +0900

    Fixed mistyped.

--- Day05/op_server.c
+++ Day05/op_server.c
@@ -101,13 +101,13 @@ int calculate(int opnum, int opnds[], char operator) {
     int result = opnds[0], i;
     switch(op) {
         case '+':
-            for(i=1; i<opnum, i++) result += opnds[i];
+            for(i=1; i<opnum; i++) result += opnds[i];
             break;
         case '-':
-            for(i=1; i<opnum, i++) result -= opnds[i];
+            for(i=1; i<opnum; i++) result -= opnds[i];
             break;
         case '*':
-            for(i=1; i<opnum, i++) result *= opnds[i];
+            for(i=1; i<opnum; i++) result *= opnds[i];
             break;
     }
 
